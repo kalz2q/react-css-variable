d497c1cf08633e102f5b1e6677d002db0c531682
kalz2q <katsu95i@gmail.com>
2022-09-11 06:05:54 +0900

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "react-css-variable"
+  })
+  const SelectElement = document.querySelector('.root');
+  const temp: Element;
+  switch SelectElement {
+    case null:
+      
+  }
+  const SelectStyle = getComputedStyle(SelectElement);
+  const StyleValue = String(SelectStyle.getPropertyValue('--my-hue')).trim();
   return (
     <div className="App">
+      <h1>タイトル</h1>
+      <p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
+      <div className="btn">OK</div>
     </div>
   );
 }
