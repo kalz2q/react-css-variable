7e668b54beded5bf506ecc6f437ea9a4695b1011
kalz2q <katsu95i@gmail.com>
2022-10-18 14:49:51 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,171 @@ import './App.css';
 const { useState } = React;
 
 function App() {
-  const [bgcolor, setBGColor] = useState("blue");
+  const [bgcolor, setBGColor] = useState(colorlist[Math.floor(Math.random() * colorlist.length)]);
 
   document.documentElement.style.setProperty('--background', bgcolor);
 
-  const changeBGColor = () => {
-    if (bgcolor === "blue") {
-      setBGColor("red");
-      document.documentElement.style.setProperty('--background', bgcolor);
-    }
-    else {
-      setBGColor("blue");
-      document.documentElement.style.setProperty('--background', bgcolor);
-    }
+  // const changeBGColor = () => {
+  //   if (bgcolor === "blue") {
+  //     setBGColor("red");
+  //     document.documentElement.style.setProperty('--background', bgcolor);
+  //   }
+  //   else {
+  //     setBGColor("blue");
+  //     document.documentElement.style.setProperty('--background', bgcolor);
+  //   }
 
-  };
+  // };
 
   return (
     <div className="App">
-      <button className="btn" onClick={() => changeBGColor()}>
-        <h3>Change background-color</h3>
-      </button>
+      <h1>Hello</h1>
     </div>
   );
 }
+
+const colorlist = [
+  "aliceblue",
+  "antiquewhite",
+  "aqua",
+  "aquamarine",
+  "azure",
+  "beige",
+  "bisque",
+  "black",
+  "blanchedalmond",
+  "blue",
+  "blueviolet",
+  "brown",
+  "burlywood",
+  "cadetblue",
+  "chartreuse",
+  "chocolate",
+  "coral",
+  "cornflowerblue",
+  "cornsilk",
+  "crimson",
+  "cyan",
+  "darkblue",
+  "darkcyan",
+  "darkgoldenrod",
+  "darkgreen",
+  "darkgrey",
+  "darkkhaki",
+  "darkmagenta",
+  "darkolivegreen",
+  "darkorange",
+  "darkorchid",
+  "darkred",
+  "darksalmon",
+  "darkseagreen",
+  "darkslateblue",
+  "darkslategrey",
+  "darkturquoise",
+  "darkviolet",
+  "deeppink",
+  "deepskyblue",
+  "dimgrey",
+  "dodgerblue",
+  "firebrick",
+  "floralwhite",
+  "forestgreen",
+  "fuchsia",
+  "gainsboro",
+  "ghostwhite",
+  "gold",
+  "goldenrod",
+  "green",
+  "greenyellow",
+  "grey",
+  "honeydew",
+  "hotpink",
+  "indianred",
+  "indigo",
+  "ivory",
+  "khaki",
+  "lavender",
+  "lavenderblush",
+  "lawngreen",
+  "lemonchiffon",
+  "lightblue",
+  "lightcoral",
+  "lightcyan",
+  "lightgoldenrodyellow",
+  "lightgreen",
+  "lightgrey",
+  "lightpink",
+  "lightsalmon",
+  "lightseagreen",
+  "lightskyblue",
+  "lightslategrey",
+  "lightsteelblue",
+  "lightyellow",
+  "lime",
+  "limegreen",
+  "linen",
+  "magenta",
+  "maroon",
+  "mediumaquamarine",
+  "mediumblue",
+  "mediumorchid",
+  "mediumpurple",
+  "mediumseagreen",
+  "mediumslateblue",
+  "mediumspringgreen",
+  "mediumturquoise",
+  "mediumvioletred",
+  "midnightblue",
+  "mintcream",
+  "mistyrose",
+  "moccasin",
+  "navajowhite",
+  "navy",
+  "oldlace",
+  "olive",
+  "olivedrab",
+  "orange",
+  "orangered",
+  "orchid",
+  "palegoldenrod",
+  "palegreen",
+  "paleturquoise",
+  "palevioletred",
+  "papayawhip",
+  "peachpuff",
+  "peru",
+  "pink",
+  "plum",
+  "powderblue",
+  "purple",
+  "rebeccapurple",
+  "red",
+  "rosybrown",
+  "royalblue",
+  "saddlebrown",
+  "salmon",
+  "sandybrown",
+  "seagreen",
+  "seashell",
+  "sienna",
+  "silver",
+  "skyblue",
+  "slateblue",
+  "slategrey",
+  "snow",
+  "springgreen",
+  "steelblue",
+  "tan",
+  "teal",
+  "thistle",
+  "tomato",
+  "turquoise",
+  "violet",
+  "wheat",
+  "white",
+  "whitesmoke",
+  "yellow",
+  "yellowgreen"
+]
 
 export default App;
